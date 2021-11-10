ba39c6d936b32b585a63a6fab77aa12b1224c238
Anton Khoda <anton.khoda@chisw.com>
2021-11-10 15:30:36 +0200

    js fix

--- src/js/payment.js
+++ src/js/payment.js
@@ -1,32 +1,30 @@
 const switchButton = document.querySelector(".js-payment-switch");
+const premiumPrice = document.querySelector(".js-premium-price");
+const premiumWebsite = document.querySelector(".js-premium-website");
+const premiumHosting = document.querySelector(".js-premium-hosting");
+const enterprisePrice = document.querySelector(".js-enterprise-price");
+const enterpriseHosting = document.querySelector(".js-enterprise-hosting");
+const pPriceValue = 29;
+const pWebsiteValue = 10;
+const pHostingValue = 15;
+const ePriceValue = 49;
+const eHostingValue = 50;
+let index;
+
 
 switchButton.addEventListener("click", function (event) {
-  const classListET = event.target.classList;
-  const premiumPrice = document.querySelector(".js-premium-price");
-  const premiumWebsite = document.querySelector(".js-premium-website");
-  const premiumHosting = document.querySelector(".js-premium-hosting");
-  const enterprisePrice = document.querySelector(".js-enterprise-price");
-  const enterpriseHosting = document.querySelector(".js-enterprise-hosting");
-
-  if (!classListET.contains("button--switch--is-active")) {
+  if (!event.target.classList.contains("button--switch--is-active")) {
     this.querySelector(".button--switch--is-active").classList.remove(
       "button--switch--is-active"
     );
-
-    classListET.add("button--switch--is-active");
-
-    if (classListET.contains("js-monthly-payment")) {
-      premiumPrice.innerHTML = premiumPrice.innerHTML * 2;
-      premiumWebsite.innerHTML = premiumWebsite.innerHTML * 2;
-      premiumHosting.innerHTML = premiumHosting.innerHTML * 2;
-      enterprisePrice.innerHTML = enterprisePrice.innerHTML * 2;
-      enterpriseHosting.innerHTML = enterpriseHosting.innerHTML * 2;
-    } else if (classListET.contains("js-yearly-payment")) {
-      premiumPrice.innerHTML = 29;
-      premiumWebsite.innerHTML = 10;
-      premiumHosting.innerHTML = 15;
-      enterprisePrice.innerHTML = 49;
-      enterpriseHosting.innerHTML = 50;
-    }
   }
+
+  event.target.classList.add("button--switch--is-active");
+
+  index = event.target.dataset.x;
+  premiumPrice.innerHTML = pPriceValue * index;
+  premiumWebsite.innerHTML = pWebsiteValue * index;
+  premiumHosting.innerHTML = pHostingValue * index;
+  enterprisePrice.innerHTML = ePriceValue * index;
+  enterpriseHosting.innerHTML = eHostingValue * index;
 });
